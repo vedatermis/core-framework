80dcce60688d1e15545e4479df5b32393c45c9e7
Vedat ERMIS <vedatermis@hotmail.com>
2020-01-22 21:23:25 +0300

    commit

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Entities.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator: AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty();
+            RuleFor(p => p.ProductName).Length(2, 30);
+        }
+    }
+}
