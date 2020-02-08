d6da7855822cc63ec1a726d34955ff10d971cb66
Vedat ERMIS <vedatermis@hotmail.com>
2020-02-09 00:06:15 +0300

    commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Business.Abstract;
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Logging;
 using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 
 namespace Business.Concrete
 {
-    public class ProductManager: IProductService
+    public class ProductManager : IProductService
     {
         private readonly IProductDal _productDal;
 
@@ -21,19 +24,20 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-        
-      public IDataResult<Product> GetById(int productId)
+
+        public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         public IDataResult<List<Product>> GetList()
         {
-            return new SuccessDataResult<List<Product>>( _productDal.GetList().ToList());
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
         }
 
-        [SecuredOperation("Product.List,Admin")]
+        //[SecuredOperation("Product.List,Admin")]
         [CacheAspect(duration: 1)]
+        [LogAspect(typeof(FileLogger))]
         public IDataResult<List<Product>> GetListByCategory(int categoryId)
         {
             return new SuccessDataResult<List<Product>>(_productDal.GetList(x => x.CategoryId == categoryId).ToList());
